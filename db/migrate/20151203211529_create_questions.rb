b8bdfde57d42c27b3c89f94f02a7a6cc0867d43d
zinosama <email": null>
2015-12-03 16:27:51 -0500

    added migrations

---
+++ db/migrate/20151203211529_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.string :title, null:false
+      t.string :body, null:false
+
+      t.references :user
+
+      t.timestamps null:false
+    end
+  end
+end
