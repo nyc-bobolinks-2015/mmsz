7ef7d651ddabe2dd687a3681292dcb3a7d294966
Sam Purcell <email": null>
2015-12-04 15:42:38 -0500

    Add new/create route to answers_controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,13 +1,26 @@
 class AnswersController < ApplicationController
   def new
-    @question = params[:question_id]
+    @question = Question.find_by(id: params[:question_id])
     @answer = Answer.new
   end
 
   def create
+    @question = Question.find_by(id: params[:question_id])
+    @answer = Answer.new(answer_params)
+    @answer.question_id = params[:question_id]
+    @answer.user_id = current_user.id
+
+    if @answer.save
+      redirect_to question_path(@question)
+    else
+      @errors = @answer.errors.full_messages
+      render :'answers/new'
+    end
   end
 
   private
 
   def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
