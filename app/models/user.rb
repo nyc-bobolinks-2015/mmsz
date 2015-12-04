d9361b9bc00da63ab7ba337bd197e95cf9041ffe
heylittlehouse <michael.r.landon@gmail.com>
2015-12-04 16:01:13 -0500

    Add method for top_three_tags for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,27 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true
   validates :username, length: {in: 1..40}
+
+  def top_three_tags
+  	occurances = occurance_hash(all_tags)
+  	ordered_hash = occurances.sort_by{|_key, value| value}.to_h
+    ordered_hash.keys.pop(3)
+  end
+
+  private
+
+  def all_tags
+  	self.questions.map do |question|
+  		question.tags
+  	end.flatten
+  end
+
+  def occurance_hash(tags_array)
+  	hash = Hash.new(0)
+  	tags_array.each do |tag|
+  		hash[tag] += 1
+  	end
+  	hash
+  end
+  
 end
