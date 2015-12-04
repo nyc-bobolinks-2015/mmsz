a4748b718691ec25e38f1931cb02918995b00a71
Sam Purcell <email": null>
2015-12-04 16:23:20 -0500

    Add edit/delete routes for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -18,6 +18,32 @@ class AnswersController < ApplicationController
     end
   end
 
+  def edit
+    @question = Question.find_by(id: params[:question_id])
+    @answer = Answer.find_by(id: params[:id])
+  end
+
+  def update
+    @question = Question.find_by(id: params[:question_id])
+    @answer = Answer.find_by(id: params[:id])
+    @answer.assign_attributes(answer_params)
+
+    if @answer.save
+      redirect_to question_path(@question)
+    else
+      @errors = @answer.errors.full_messages
+      render :'answers/edit'
+    end
+  end
+
+  def destroy
+    @question = Question.find_by(id: params[:question_id])
+    answer = Answer.find_by(id: params[:id])
+    answer.destroy
+
+    redirect_to question_path(@question)
+  end
+
   private
 
   def answer_params
