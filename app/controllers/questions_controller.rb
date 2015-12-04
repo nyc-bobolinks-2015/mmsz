c54f31a13d0416c0ee4f36e99af7bcd6775b5684
Sam Purcell <email": null>
2015-12-04 16:23:44 -0500

    Add edit/delete options for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -26,8 +26,30 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def edit
+    @question = Question.find_by(id: params[:id])
+  end
+
+  def update
+    @question = Question.find_by(id: params[:id])
+    @question.assign_attributes(question_params)
+
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      @errors = @question.errors.full_messages
+      render :'questions/edit'
+    end
+  end
+
   def show
     @question = Question.find_by(id: params[:id])
+  end
+
+  def destroy
+    question = Question.find_by(id: params[:id])
+    question.destroy
+    redirect_to questions_path
   end
 
   private
