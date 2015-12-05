dc69bde716f0272f69b8478e75ec9446a3bef61b
heylittlehouse <michael.r.landon@gmail.com>
2015-12-05 16:08:46 -0500

    Add before method that logs a user in

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 feature 'User browsing the website' do
 	before :each do
+		user = FactoryGirl.create(:user)
+		visit login_path
+		fill_in "password", with: user.password
+		fill_in "email", with: user.email
+		click_button "Log in"
 		stub_authorize_user!
 		5.times{FactoryGirl.create(:question)}
 	end
@@ -11,5 +16,11 @@ feature 'User browsing the website' do
       visit root_path
       expect(page).to have_css('li')
     end
+
+    it "sees a Logout button when logged in" do
+    	visit root_path
+    	expect(page).to have_text('Logout')
+    end
+
 	end
 end
