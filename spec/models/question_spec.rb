05b19c8f6e300612335b54151f959f4c5f02a327
Sam Purcell <email": null>
2015-12-05 14:52:47 -0500

    Add model tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,4 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  context "validations" do
+    it { should validate_presence_of :body }
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :title }
+    it do
+      should validate_length_of(:title).
+      is_at_least(1).
+      is_at_most(400)
+    end
+    it do
+      should validate_length_of(:body).
+      is_at_least(1).
+      is_at_most(400)
+    end
+  end
+
+  it "should save all fields if question is valid" do
+    question = Question.new
+    question.title = "What is up?"
+    question.body = "This is the body for a question"
+    question.user = FactoryGirl.create(:user)
+
+    expect {
+      question.save
+    }.to change{question.id}.from(nil).to be_truthy
+  end
 end
