4e8f73c3bd91bb0b5e92f59662482b0e3e41adbd
heylittlehouse <michael.r.landon@gmail.com>
2015-12-05 16:29:41 -0500

    redirects to current user profile page from layout list

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 feature 'User browsing the website' do
 	before :each do
 		user = FactoryGirl.create(:user)
-		visit login_path
-		fill_in "password", with: user.password
-		fill_in "email", with: user.email
-		click_button "Log in"
-		stub_authorize_user!
+		# visit login_path
+		# fill_in "password", with: user.password
+		# fill_in "email", with: user.email
+		# click_button "Log in"
+		stub_current_user(user)
 		5.times{FactoryGirl.create(:question)}
 	end
 
@@ -22,5 +22,19 @@ feature 'User browsing the website' do
     	expect(page).to have_text('Logout')
     end
 
+    it "sees its name on the page" do
+    	user = FactoryGirl.create(:user)
+    	stub_current_user(user)
+    	visit root_path
+    	expect(page).to have_text("#{user.username}")
+    end
+
+    it "goes to its profile page from the posts index" do
+    	user = FactoryGirl.create(:user)
+    	stub_current_user(user)
+    	visit root_path
+    	click_link("#{user.username}")
+    	expect(current_path).to eq(user_path(user))
+    end
 	end
 end
