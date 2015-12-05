d251687a76f550f51d4e4f23e4b31fa5daa34717
heylittlehouse <michael.r.landon@gmail.com>
2015-12-05 14:33:15 -0500

    Add user authentication methods for rspec

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 feature 'User browsing the website' do
 	before :each do
 		stub_authorize_user!
+		5.times{FactoryGirl.create(:question)}
 	end
 
+	context "on hompage" do
+		it "sees a list of recent question titles" do
+      visit root_path
+      expect(page).to have_css('li')
+    end
+	end
 end
