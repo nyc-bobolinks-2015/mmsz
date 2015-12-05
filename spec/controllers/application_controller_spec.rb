84428eb7f18d9f1d3da8b4a03d8d84c1f57c0c8c
Sam Purcell <email": null>
2015-12-05 16:50:29 -0500

    Add tests to application controller

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  let!(:user) {FactoryGirl.create(:user)}
+
+  describe "#logged_in?" do
+    context "with a logged in user" do
+
+      before do
+        session[:user_id] = user.id
+      end
+
+      it "returns true" do
+        expect(controller.send(:logged_in?)).to eq true
+      end
+    end
+
+    context "with no user logged in" do
+      it "returns false" do
+        expect(controller.send(:logged_in?)).to eq false
+      end
+    end
+  end
+
+  describe "#current_user" do
+    context "with a logged in user" do
+
+      before do
+        session[:user_id] = user.id
+      end
+
+      it "returns the user that is logged in" do
+        expect(controller.send(:current_user)).to eq user
+      end
+    end
+
+    context "with no user logged in" do
+      it "returns nil" do
+        expect(controller.send(:current_user)).to eq nil
+      end
+    end
+  end
+
+  describe "#authorize_user!" do
+    context "with a logged in user" do
+      it "redirects "
+    end
+  end
+
+
+end
