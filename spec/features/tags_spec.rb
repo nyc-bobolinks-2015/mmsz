0e8baa9e3231dea28f7db365e6efd91a6c236d96
Sam Purcell <email": null>
2015-12-05 18:41:41 -0500

    Fix tags_spec

--- spec/features/tags_spec.rb
+++ spec/features/tags_spec.rb
@@ -1,55 +1,56 @@
 require 'rails_helper'
 
-feature 'Tags index page' do 
-  
+feature 'Tags index page' do
+
   before :each do
     user = FactoryGirl.create(:user)
       stub_current_user(user)
       5.times{FactoryGirl.create(:tag)}
   end
 
-  scenario 'tags link links to tags/index page from root_path' do 
+  scenario 'tags link links to tags/index page from root_path' do
     visit root_path
     click_link("Tags")
     expect(current_path).to eq tags_path
-  end 
+  end
 
-  scenario 'tags link to thier show page from index_page' do 
+  scenario 'tags link to thier show page from index_page' do
     visit tags_path
     click_link(Tag.last.name)
     expect(current_path).to eq tag_path(Tag.last)
-  end 
+  end
 
-  scenario 'search by name form takes you to search route' do 
+  scenario 'search by name form takes you to search route' do
     visit tags_path
     fill_in 'search_name', with: Tag.last.name
     click_button("Search By Tag")
     expect(current_path).to eq "/tags/search"
-  end 
+  end
 
 end
 
-feature 'Tags show page' do 
-  
+feature 'Tags show page' do
+
   before :each do
-      stub_authorize_user!
+    user = FactoryGirl.create(:user)
+      stub_current_user user
       5.times{FactoryGirl.create(:tag)}
   end
 
-  scenario 'tag is displayed under questions for that tag' do 
+  scenario 'tag is displayed under questions for that tag' do
     visit tag_path(Tag.first)
     expect(page).to have_content Tag.first.name
   end
 
-  scenario 'vote count for questions are displayed' do 
+  scenario 'vote count for questions are displayed' do
     visit tag_path(Tag.first)
     expect(page).to have_content Tag.first.questions.first.vote_count
-   
+
   end
 
-  scenario 'answer count for questions are displayed' do 
+  scenario 'answer count for questions are displayed' do
     visit tag_path(Tag.first)
-    expect(page).to have_content Tag.first.questions.first.answers.count 
+    expect(page).to have_content Tag.first.questions.first.answers.count
   end
 
 end
